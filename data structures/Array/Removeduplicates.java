00699d5519c89d353e8fca1682aa04ecd6a472df
jusvanth-17 <jusvanth475@gmail.com>
2024-09-25 18:27:59 +0530

    duplicates

--- data structures/Array/Removeduplicates.java
+++ data structures/Array/Removeduplicates.java
@@ -1,23 +1,34 @@
-import java.util.TreeSet;
-
-class Removeduplicates {
-    static int removeDuplicates(int[] nums) {
-        TreeSet<Integer> set = new TreeSet<>();
-
-        for (int num : nums) {
-            set.add(num);
+class  Removeduplicates {
+    public static int removeDuplicates(int[] nums) {
+        if (nums.length == 0) {
+            return 0; 
         }
 
-        int index = 0;
-        for (int num : set) {
-            nums[index++] = num;
+        int j = 1; 
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[j] = nums[i]; 
+               j++;
+            }
         }
 
-        return set.size();
-
+        return j; // Return the number of unique elements
     }
+
+    // Main method to test the removeDuplicates function
     public static void main(String[] args) {
-        int nums[]={1,2,3,3};
+
+        // Example input array
+        int[] nums = {1, 1, 2, 3, 4, 4, 5, 6, 6};
+
+        // Print original array
+        System.out.println("Original Array:");
+        for (int num : nums) {
+            System.out.print(num + " ");
+        }
+
+        // Call the removeDuplicates method
         int uniqueCount = removeDuplicates(nums);
 
         System.out.println("\n\nArray after removing duplicates:");
@@ -25,6 +36,7 @@ class Removeduplicates {
             System.out.print(nums[i] + " ");
         }
 
+        // Print the count of unique elements
         System.out.println("\n\nNumber of unique elements: " + uniqueCount);
     }
 }

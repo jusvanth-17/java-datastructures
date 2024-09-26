6f54861c9fec1c1f517479e37b46c46ae77c47d0
jusvanth-17 <jusvanth475@gmail.com>
2024-09-26 10:24:49 +0530

    file added

---
+++ data structures/Array/Mayri.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Mayri {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        String cat;
+        int price;
+        int discount;
+        for(int i=0;i<n;i++){
+            cat=in.next();
+            price=in.nextInt();
+            discount=in.nextInt();
+        }
+        
+
+    }
+}
